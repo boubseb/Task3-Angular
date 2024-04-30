737578dab92c01a0cd8cec5ef1b763d2f735dae9
Sebastien Peltier <sebpel62@gmail.com>
2024-04-30 18:00:51 +0200

    task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from './block/block.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    BlockComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  blockCount: number[] = [0, 1, 2, 3];
   title = 'firstApp';
 }
